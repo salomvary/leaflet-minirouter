ec58c296536b330693cd3598787523c2cd1af22a
Márton Salomváry <salomvary@gmail.com>
2020-04-12 11:49:53 +0200

    Build declaration files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,53 @@
 import L from "leaflet";
 import Layer, { LayerOptions } from "./layer";
-import { OSRMV1, Mapbox, OSRMV1Options, MapboxOptions } from "./router";
+import {
+  Mapbox,
+  MapboxOptions,
+  OSRMV1,
+  OSRMV1Options,
+  Route,
+  Router,
+} from "./router";
 
-(<any>L).miniRouter = {
+declare module "leaflet" {
+  namespace miniRouter {
+    function layer(options?: LayerOptions): Layer;
+    function osrmv1(options: OSRMV1Options): OSRMV1;
+    function mapbox(accessToken: string, options: MapboxOptions): Mapbox;
+  }
+
+  // TODO: figure out how not to duplicate classes here
+
+  namespace MiniRouter {
+    class Layer extends L.Layer {
+      constructor(options?: LayerOptions);
+      onAdd(map: L.Map): this;
+      onRemove(map: L.Map): this;
+      route(): Promise<void>;
+      getWaypoints(): L.LatLng[];
+      getCoordinates(): L.LatLng[];
+    }
+
+    class OSRMV1 implements Router {
+      constructor({
+        serviceUrl,
+        profile,
+        requestParameters,
+      }?: {
+        serviceUrl?: string;
+        profile?: string;
+        requestParameters?: {};
+      });
+      route(waypoints: L.LatLng[]): Promise<Route[]>;
+    }
+
+    class Mapbox extends OSRMV1 {
+      constructor(accessToken: string, { serviceUrl, profile }?: MapboxOptions);
+    }
+  }
+}
+
+L.miniRouter = {
   layer(options?: LayerOptions) {
     return new Layer(options);
   },
@@ -14,8 +59,8 @@ import { OSRMV1, Mapbox, OSRMV1Options, MapboxOptions } from "./router";
   },
 };
 
-(<any>L).MiniRouter = {
+L.MiniRouter = {
   Layer: Layer,
-  MapBox: Mapbox,
+  Mapbox: Mapbox,
   OSRMV1: OSRMV1,
 };
