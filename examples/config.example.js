ad61c82c078c42c47c2a39e24d78b9f3c4968b32
Márton Salomváry <salomvary@gmail.com>
2020-04-23 21:39:30 +0200

    Remove real config

---
+++ examples/config.example.js
@@ -0,0 +1,6 @@
+/* Copy this file to config.js and add your API keys */
+
+miniRouterConfig = {
+  mapboxKey: "<your api key>",
+  graphHopperKey: "<your api key>",
+};
--- examples/config.js
+++
@@ -1,5 +0,0 @@
-miniRouterConfig = {
-  mapboxKey:
-    "pk.eyJ1Ijoic2Fsb212YXJ5IiwiYSI6ImNpcWI1Z21lajAwMDNpMm5oOGE4ZzFzM3YifQ.DqyC3wn8ChEjcztfbY0l_g",
-  graphHopperKey: "cd462023-b872-4db6-b5cd-aad62847c8b7",
-};
